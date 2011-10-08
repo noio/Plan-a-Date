93ff81ed8b6089d8c60f0d749582ad3fea1abc34
Thomas van den Berg <paranoid.ndroid@gmail.com>
2011-10-08 17:35:56 +0200

    add admin_required decorator

--- appengine/planadate/views.py
+++ appengine/planadate/views.py
@@ -30,7 +30,18 @@ import models
 ### Constants ###
 
 ### Decorators for Request Handlers ###
-
+def admin_required(func):
+    """Decorator that insists that you're logged in as administrator."""
+    
+    def admin_wrapper(request, *args, **kwds):
+        if request.user is None:
+            return HttpResponseRedirect(
+                users.create_login_url(request.get_full_path().encode('utf-8')))
+        if not request.user_is_admin:
+            return HttpResponseForbidden('You must be admin for this function')
+        return func(request, *args, **kwds)
+    
+    return admin_wrapper
 
 ### Page Handlers ###
 
